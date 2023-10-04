a929f4ef2e1bcd04cc98259bbca173f62ad9175d
Adam <agorring@iszolea.net>
2023-10-04 19:38:03 +1000

    Fixed the context issue. App presents well.

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -6,10 +6,10 @@ const Overview = ({ symbol, price, change, changePercentage, currency }) => {
       <span className="absolute left-4 top-4 text-neutral-400 text-lg xl:text-xl 2xl:text-2xl">
         {symbol}
       </span>
-      <div className="w-full h-full flex items-center justify-around">
-        <span className="text-2xl xl:text-4xl 2xl:text-5xl flex items-center">
+      <div className="w-full h-full flex items-center justify-around p-5">
+        <span className="text-2xl 2xl:text-5xl flex items-center">
           ${price}
-          <span className="text-lg xl:text-xl 2xl:text-2xl text-neutral-400 m-2">
+          <span className="text-lg 2xl:text-2xl text-neutral-400 m-2">
             {currency}
           </span>
         </span>
--- src/components/StateProvider.js
+++ src/components/StateProvider.js
@@ -1,17 +1,13 @@
 import React, { useState } from "react";
-//import ThemeContext from "./context/ThemeContext";
 import StockContext from "../context/StockContext";
 
 const StateProvider = ({ children }) => {
-  //const [darkMode, setDarkMode] = useState(false);
   const [stockSymbol, setStockSymbol] = useState("AAPL");
 
   return (
-    /*<ThemeContext.Provider value={{ darkMode, setDarkMode }}>*/
     <StockContext.Provider value={{ stockSymbol, setStockSymbol }}>
       {children}
     </StockContext.Provider>
-    /*</ThemeContext.Provider>*/
   );
 };
 
